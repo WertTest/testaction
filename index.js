aefb56fbdde631940a444f9c44f13948cbdde65f
Alexander Wert <alexander.wert@elastic.co>
2022-04-03 15:56:02 +0200

    Init

---
+++ index.js
@@ -0,0 +1,75 @@
+const core = require("@actions/core");
+const { request } = require("@octokit/request");
+const github = require("@actions/github");
+
+function parse_array(input_name) {
+  const input_value = core.getInput(input_name)
+  if (input_value === "") {
+    return undefined; 
+  }
+  if (input_value === "<<EMPTY>>") {
+    return [];
+  }
+  return input_value.split(",");
+}
+
+function parse_boolean(input_name) {
+  const input_value = core.getInput(input_name);
+  return input_value === "true";
+}
+
+function parse_object(input_name) {
+  const input_value = core.getInput(input_name);
+  return JSON.parse(input_value);
+}
+
+function default_parse(input_name) {
+  const input_value = core.getInput(input_name);
+  if (!input_value) {
+    if (input_name === 'owner') {
+      return github.context.repo.owner;
+    } else if (input_name === 'repo') {
+      return github.context.repo.repo;
+    }
+  }
+  return input_value || undefined;
+}
+
+const metaIssue = parse_object("metaIssue");
+const token = default_parse("token");
+
+const owner = "WertTest";
+const repo = "RepoB";
+
+const requestWithAuth = request.defaults({
+  headers: {
+    authorization: `Bearer ${token}`
+  }
+  // ,
+  // mediaType: {
+  //   previews: [
+  //     "symmetra",
+  //   ]
+  // } 
+});
+
+requestWithAuth("post /repos/{owner}/{repo}/issues", {
+    token,
+    owner,
+    repo,
+    title: `[META ${metaIssue.number}] ${metaIssue.title}`,
+    body: "testbody",
+})
+  .then(result => {
+    console.log("result", result);
+    if (result && result.data && result.data.id) {
+      core.setOutput('id', result.data.id)
+    }
+    if (result && result.data && result.data.number) {
+      core.setOutput('number', result.data.number)
+    }
+  })
+  .catch(error => {
+    console.log("error", error);
+    core.setFailed(error.message);
+  });

576f0f6b38228031d161b0f9e4f0aae8c16db3d7
Alexander Wert <alexander.wert@elastic.co>
2022-04-04 08:47:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,42 +36,24 @@ function default_parse(input_name) {
 }
 
 const metaIssue = parse_object("metaIssue");
+const metaIssueRepo = metaIssue.repository_url.match(/https.*\/repos\/[^\/]*\/(.*)/)[1];
+const owner = metaIssue.repository_url.match(/https.*\/repos\/([^\/]*)\/.*/)[1];
 const token = default_parse("token");
-
-const owner = "WertTest";
-const repoRegex = /.*<!---repos-start--->(.*)<!---repos-end--->.*/ms;
+const bodyRegex = /(.*)(<!---repos-start--->.*<!---repos-end--->)(.*)/ms;
 const extractReposRegex = /- \[.\] ([^\r]*)/ms;
-
-const retrieveRepos = (body) => {
-  const reposBody = body.match(repoRegex)[1];
-  if(reposBody){
-    return reposBody.split('\r\n')
-                              .filter(line => line.startsWith('- ['))
-                              .map(line => line.match(extractReposRegex)[1]);
-  }
-  return [];
-};
 
 const requestWithAuth = request.defaults({
   headers: {
     authorization: `Bearer ${token}`
   }
-  // ,
-  // mediaType: {
-  //   previews: [
-  //     "symmetra",
-  //   ]
-  // } 
 });
 
-
-
-const createIssue = (repo) => requestWithAuth("post /repos/{owner}/{repo}/issues", {
+const createIssue = (repo, title, body) => requestWithAuth("post /repos/{owner}/{repo}/issues", {
   token,
   owner,
   repo,
-  title: `[META ${metaIssue.number}] ${metaIssue.title}`,
-  body: "testbody",
+  title,
+  body,
 })
 .then(result => {
   return result;
@@ -80,17 +62,63 @@ const createIssue = (repo) => requestWithAuth("post /repos/{owner}/{repo}/issues
   console.log("error", error);
 });
 
+const updateMetaIssue = async (agentIssues, specIssue, oldBody) => {
+  var overviewSection = "";
+  if (specIssue) {
+    overviewSection = overviewSection + "## Spec Issue\r\n";
+    overviewSection = overviewSection + `- [ ] https://github.com/${specIssue}\r\n`;
+    overviewSection = overviewSection + "\r\n";
+  }
+  if(agentIssues.length > 0){
+    overviewSection = overviewSection + "## Agent Issues\r\n";
+    for (const aIssue of agentIssues) {
+      overviewSection = overviewSection + `- [ ] https://github.com/${aIssue}\r\n`;
+    }
+  }
+
+  const newBody = oldBody + overviewSection;
+  await requestWithAuth("patch /repos/{owner}/{repo}/issues/{issue_number}", {
+    token,
+    owner,
+    repo: metaIssueRepo,
+    issue_number: metaIssue.number,
+    body: newBody,
+  })
+  .then(result => {
+    return result;
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
+};
 
 const run = async () => {
   const createdIssues = [];
-  const repos = retrieveRepos(metaIssue.body);
-  for (const repo of repos) {
-    const response = await createIssue(repo);
-    console.log('NR', JSON.stringify(createdIssues));
-    createdIssues.push(`${repo}/${response.data.number}`);
+  const bodyMatchArray = metaIssue.body.match(bodyRegex);
+  if(bodyMatchArray || bodyMatchArray.length === 4){
+    const repos = bodyMatchArray[2].split('\n').filter(line => line.startsWith('- [x]')).map(line => line.match(extractReposRegex)[1].trim());
+    
+    var specIssueNumber = undefined;
+    if(repos.some(r = r.toLowerCase().startsWith('spec'))){
+      const specIssueBody = `See meta issue for the description:\r\n- [ ] ${metaIssue.html_url}`
+      const specResponse = await createIssue(metaIssueRepo, `[META ${metaIssue.number}] Spec: ${metaIssue.title}`, specIssueBody);
+      specIssueNumber = specResponse.data.number;
+    }
+
+    const subIssueBody = `See meta issue${specIssueNumber ? ' and spec ' : ' '}for the description and details:\r\n- [ ] Meta issue: ${metaIssue.html_url}\r\n`
+          + (specIssueNumber ? `- [ ] Spec issue: https://github.com/${owner}/${metaIssueRepo}/issues/${specIssueNumber}\r\n` : '');
+    for (const repo of repos) {
+      const response = await createIssue(repo, `[META ${metaIssue.number}] ${metaIssue.title}`, subIssueBody);
+      createdIssues.push(`${owner}/${repo}/issues/${response.data.number}`);
+    }
+
+    const metaIssueBody = `${bodyMatchArray[1]}\r\n${bodyMatchArray[3]}\r\n\r\n`;
+    const specIssue = specIssueNumber ? `${owner}/${metaIssueRepo}/issues/${specIssueNumber}` : undefined;
+    await updateMetaIssue(createdIssues, specIssue, metaIssueBody);
+  } else {
+    console.log("No issue creation list identified. Skipping execution.", error);
   }
 
-  console.log('OUTPUT', JSON.stringify(createdIssues));
   core.setOutput('createdIssues', JSON.stringify(createdIssues));
 };
 
